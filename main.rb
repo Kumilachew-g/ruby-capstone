02273d0cc756ce60759677f807d325169f8af0b4
kumilachew-g <kumilachew1212@gmail.com>
2022-11-20 11:44:42 +0300

    Make enhance

--- main.rb
+++ main.rb
@@ -22,7 +22,7 @@ class Main
     puts
     puts '1-  List all books'
     puts '2-  List all music albums'
-    puts '3-  List of games'
+    puts '3-  List all games'
     puts '4-  List all genres'
     puts '5-  List all labels'
     puts '6-  List all authors'
